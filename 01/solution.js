5d2440815270df060f0a1ab58006af7d20414107
gr <prodbygr@hotmail.es>
2021-12-03 19:41:32 -0400

    add day 01

---
+++ 01/solution.js
@@ -0,0 +1,32 @@
+const input = require("./input");
+
+function part1(depths) {
+  let increments = 0;
+
+  depths.forEach((depth, index) => {
+    if (index === 0) return;
+
+    if (depth > depths[index - 1]) increments++;
+  });
+
+  return increments;
+}
+
+function part2(depths) {
+  let increments = 0;
+  let prevSum = depths[0] + depths[1] + depths[2];
+
+  depths.forEach((depth, index) => {
+    if (index < 3) return;
+
+    let currentSum = depths[index] + depths[index - 1] + depths[index - 2];
+
+    if (currentSum > prevSum) increments++;
+
+    prevSum = currentSum;
+  });
+
+  return increments;
+}
+
+console.log(`Part 1: ${part1(input)}\nPart 2: ${part2(input)}`);
